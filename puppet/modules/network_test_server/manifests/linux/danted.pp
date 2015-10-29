01f4b9aa4456ea2fe0fc0598af76ad6596ac0ea3
Tony Sarajärvi <tony.sarajarvi@theqtcompany.com>
2015-10-29 12:18:13 +0200

    Dante to support Ubuntu 12+

--- puppet/modules/network_test_server/manifests/linux/danted.pp
+++ puppet/modules/network_test_server/manifests/linux/danted.pp
@@ -1,67 +1,90 @@
 class network_test_server::linux::danted {
 
-    if $::operatingsystem == "Ubuntu" {
-        # FIXME: tests were written against this ancient dante-server,
-        # and they now fail against current versions.
-        # For compatibility with the pre-puppet test server, we'll use this
-        # old version too, but the tests should really be fixed.
-        $dante_deb = $architecture ? {
-            i386    =>  'dante-server_1.1.14-2_i386.deb',
-            x86_64  =>  'dante-server_1.1.14-2_ia64.deb',
-            default =>  err("architecture $architecture is not supported!"),
+
+    if ($::operatingsystem == "Ubuntu") and ($lsbmajdistrelease >= 12) {
+        source_install { "danted" :
+            creates => "/usr/sbin/socksd",
+            url => "http://www.inet.no/dante/files/dante-1.2.0.tar.gz",
+            configureparams => "",
         }
 
-        exec { "fetch old dante-server":
-            creates =>  "/tmp/$dante_deb",
-            command =>  "/usr/bin/wget -O /tmp/$dante_deb http://snapshot.debian.org/archive/debian/20050312T000000Z/pool/main/d/dante/$dante_deb",
+
+        service {
+            "danted":
+                enable  =>  true,
+                ensure  =>  running,
+                require =>  Source_Install["danted"],
+            ;
+            "danted-authenticating":
+                enable  =>  true,
+                ensure  =>  running,
+                require =>  Source_Install["danted"],
+            ;
         }
 
-        package { "dante-server":
-            provider    =>  dpkg,
-            ensure      =>  latest,
-            source      =>  "/tmp/$dante_deb",
-            require     =>  [ Exec["fetch old dante-server"], File["/etc/init.d/danted-authenticating"] ],
+        file {
+            "/etc/danted.conf":
+                source  =>  "puppet:///modules/network_test_server/config/danted/danted.conf",
+                require =>  Source_Install["danted"],
+                notify  =>  Service["danted"],
+            ;
+            "/etc/danted-authenticating.conf":
+                source  =>  "puppet:///modules/network_test_server/config/danted/danted-authenticating.conf",
+                require =>  Source_Install["danted"],
+                notify  =>  Service["danted-authenticating"],
+            ;
+            "/etc/init.d/danted":
+                source  =>  "puppet:///modules/network_test_server/init/sockd",
+            ;
+            "/etc/init.d/danted-authenticating":
+                source  =>  "puppet:///modules/network_test_server/init/sockd-authenticating",
+            ;
         }
-    }
-    else {
-        # Note: OS other than Ubuntu are also likely to hit the above issue.
-        # Fix it as needed.
+    } else {
         package { "dante-server":
             ensure  =>  present,
             require =>  File["/etc/init.d/danted", "/etc/init.d/danted-authenticating"],
         }
-    }
 
-    service {
-        "danted":
-            enable  =>  true,
-            ensure  =>  running,
-            require =>  Package["dante-server"],
-        ;
-        "danted-authenticating":
-            enable  =>  true,
-            ensure  =>  running,
-            require =>  Package["dante-server"],
-        ;
-    }
+        service {
+            "danted":
+                enable  =>  true,
+                ensure  =>  running,
+                require =>  Package["dante-server"],
+            ;
+            "danted-authenticating":
+                enable  =>  true,
+                ensure  =>  running,
+                require =>  Package["dante-server"],
+            ;
+        }
 
-    file {
-        "/etc/danted.conf":
-            source  =>  "puppet:///modules/network_test_server/config/danted/danted.conf",
-            require =>  Package["dante-server"],
-            notify  =>  Service["danted"],
-        ;
-        "/etc/danted-authenticating.conf":
-            source  =>  "puppet:///modules/network_test_server/config/danted/danted-authenticating.conf",
-            require =>  Package["dante-server"],
-            notify  =>  Service["danted-authenticating"],
-        ;
-        "/etc/init.d/danted":
-            source  =>  "puppet:///modules/network_test_server/init/danted",
-        ;
-        "/etc/init.d/danted-authenticating":
-            source  =>  "puppet:///modules/network_test_server/init/danted",
-        ;
+        file {
+            "/etc/danted.conf":
+                source  =>  "puppet:///modules/network_test_server/config/danted/danted.conf",
+                require =>  Package["dante-server"],
+                notify  =>  Service["danted"],
+            ;
+            "/etc/danted-authenticating.conf":
+                source  =>  "puppet:///modules/network_test_server/config/danted/danted-authenticating.conf",
+                require =>  Package["dante-server"],
+                notify  =>  Service["danted-authenticating"],
+            ;
+            "/etc/init.d/danted":
+                source  =>  "puppet:///modules/network_test_server/init/danted",
+            ;
+            "/etc/init.d/danted-authenticating":
+                source  =>  "puppet:///modules/network_test_server/init/danted-authenticating",
+            ;
+        }
+    }
+    if ($::operatingsystem == "Ubuntu") and ($lsbmajdistrelease >= 12) {
+        file {
+            "/lib/x86_64-linux-gnu/libc.so":
+                ensure =>   'link',
+                target =>   "/lib/x86_64-linux-gnu/libc.so.6",
+            ;
+        }
     }
 
 }
